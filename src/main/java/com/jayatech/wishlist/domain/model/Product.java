5aef9d2b63ba515a6ea7c8deca34e7285d1c0b42
Romulo Pereira <romulo.pereiracz@gmail.com>
2023-09-28 08:39:50 -0300

    feat: adding first version of the project

---
+++ src/main/java/com/jayatech/wishlist/domain/model/Product.java
@@ -0,0 +1,27 @@
+package com.jayatech.wishlist.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "product")
+public class Product {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private String description;
+}
