578c781843d8400db4689d748addce2f163058e4
Romulo Pereira <romulo.pereiracz@gmail.com>
2023-09-28 22:44:54 -0300

    fix: improving productService to get better responses

--- src/main/java/com/jayatech/wishlist/domain/service/ProductService.java
+++ src/main/java/com/jayatech/wishlist/domain/service/ProductService.java
@@ -1,30 +1,36 @@
 package com.jayatech.wishlist.domain.service;
 
-import com.jayatech.wishlist.domain.exception.ProductNotFoundException;
+import com.jayatech.wishlist.domain.exception.ResourceNotFoundException;
 import com.jayatech.wishlist.domain.model.Product;
 import com.jayatech.wishlist.domain.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
 public class ProductService {
 
+    public static final String PRODUCT_NOT_FOUND_EXCEPTION_MESSAGE = Product.class.getName() +".not.found";
+
+    private final ProductRepository productRepository;
+
     @Autowired
-    private ProductRepository productRepository;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public List<Product> findAll() {
         return productRepository.findAll();
     }
 
-    public Optional<Product> findById(String id) {
-        return productRepository.findById(id);
+    public Product findById(String id) {
+        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(PRODUCT_NOT_FOUND_EXCEPTION_MESSAGE));
     }
+
+    //Remove that later
     public Product insert (Product product){
         return productRepository.save(product);
     }
