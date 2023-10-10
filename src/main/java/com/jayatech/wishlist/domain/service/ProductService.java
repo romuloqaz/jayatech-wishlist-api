e1053a2fdb7c3505d6d2e5c84a5b23f703c47832
Romulo Pereira <romulo.pereiracz@gmail.com>
2023-10-10 11:40:27 -0300

    fix: removing unused log

--- src/main/java/com/jayatech/wishlist/domain/service/ProductService.java
+++ src/main/java/com/jayatech/wishlist/domain/service/ProductService.java
@@ -3,14 +3,12 @@ package com.jayatech.wishlist.domain.service;
 import com.jayatech.wishlist.domain.exception.ResourceNotFoundException;
 import com.jayatech.wishlist.domain.model.Product;
 import com.jayatech.wishlist.domain.repository.ProductRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@Slf4j
 public class ProductService {
 
     public static final String PRODUCT_NOT_FOUND_EXCEPTION_MESSAGE = Product.class.getName() + ".not.found";
